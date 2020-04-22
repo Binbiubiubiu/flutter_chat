9b83ffb821e35b3f217d1e6d616c7bcd65505ca2
Simon-Bin <2632003786@qq.com>
2020-04-23 01:04:41 +0800

    :sparkles: 完成了通讯录索引功能

--- lib/mock/people.dart
+++ lib/mock/people.dart
@@ -1,29 +1,169 @@
 List<Map<String, Object>> peopleMock = [
-  {"group": "A"},
   {
-    "nickName": "A测试1",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+    "group": "A",
+    "people": [
+      {
+        "nickName": "A测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "A测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
   },
   {
-    "nickName": "A测试2",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
-  },
-  {"group": "B"},
-  {
-    "nickName": "B测试1",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
-  },
-  {"group": "C"},
-  {
-    "nickName": "C测试1",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+    "group": "B",
+    "people": [
+      {
+        "nickName": "B测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
   },
   {
-    "nickName": "C测试2",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+    "group": "C",
+    "people": [
+      {
+        "nickName": "C测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "C测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "C测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "C测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "C测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "C测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
   },
   {
-    "nickName": "C测试3",
-    "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+    "group": "D",
+    "people": [
+      {
+        "nickName": "D测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "D测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "D测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "D测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "D测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "D测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
+  },
+  {
+    "group": "E",
+    "people": [
+      {
+        "nickName": "E测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "E测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "E测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "E测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "E测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "E测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
+  },
+  {
+    "group": "F",
+    "people": [
+      {
+        "nickName": "F测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "F测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "F测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "F测试1",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "F测试2",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "F测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
+  },
+  {
+    "group": "H",
+    "people": [
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+      {
+        "nickName": "H测试3",
+        "avatar": "https://avatars2.githubusercontent.com/u/7017767?s=40&v=4"
+      },
+    ]
   },
 ];
--- lib/tabs/people.dart
+++ lib/tabs/people.dart
@@ -1,5 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:simonchat/mock/people.dart';
+import 'dart:math' as Math;
+
+List<String> _indexWord = [
+  "↑",
+  "☆",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "#"
+];
+
+const WORD_SIZE = 18.0;
+
+const TAIL_SIZE = 56.0;
+
+const GROUP_WORD_SIZE = 30.0;
+
+Map<String, dynamic> computedGroupTop() {
+  double hasTop = TAIL_SIZE * 4;
+  Map<String, dynamic> heightList = {
+    '↑': {"top": 0.0, "height": 0.0},
+    '☆': {"top": 0.0, "height": 0.0},
+  };
+  for (var i = 0; i < peopleMock.length; i++) {
+    var group = peopleMock[i];
+
+    double height =
+        GROUP_WORD_SIZE + (group["people"] as List<dynamic>).length * TAIL_SIZE;
+    heightList[group["group"]] = {"top": hasTop, "height": height};
+    hasTop += height;
+  }
+  return heightList;
+}
 
 class PeoplePage extends StatefulWidget {
   @override
@@ -7,11 +64,37 @@ class PeoplePage extends StatefulWidget {
 }
 
 class _PeoplePageState extends State<PeoplePage> {
+  GlobalKey _myKey = new GlobalKey();
+  ScrollController _controller = new ScrollController();
+  Map<String, dynamic> _heights;
+  List<dynamic> _listItem = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      _heights = computedGroupTop();
+      peopleMock.forEach((group) {
+        _listItem.add({"group": group["group"]});
+        _listItem.addAll(group["people"]);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         CustomScrollView(
+          key: _myKey,
+          controller: _controller,
           slivers: <Widget>[
             SliverToBoxAdapter(
               child: Column(
@@ -114,8 +197,8 @@ class _PeoplePageState extends State<PeoplePage> {
             SliverList(
               delegate: SliverChildBuilderDelegate(
                 (BuildContext context, int index) {
-                  if (peopleMock[index]["group"] != null) {
-                    return buildWordBlock(peopleMock[index]["group"]);
+                  if (_listItem[index]["group"] != null) {
+                    return buildWordBlock(_listItem[index]["group"]);
                   }
 
                   return DecoratedBox(
@@ -124,12 +207,12 @@ class _PeoplePageState extends State<PeoplePage> {
                     ),
                     child: ListTile(
                       leading: Container(
-                        width: 34.0,
-                        height: 34.0,
+                        width: 30.0,
+                        height: 30.0,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
-                            image: NetworkImage(peopleMock[index]["avatar"]),
+                            image: NetworkImage(_listItem[index]["avatar"]),
                             fit: BoxFit.contain,
                           ),
                         ),
@@ -138,7 +221,7 @@ class _PeoplePageState extends State<PeoplePage> {
                     ),
                   );
                 },
-                childCount: peopleMock.length,
+                childCount: _listItem.length,
               ),
             )
           ],
@@ -149,54 +232,54 @@ class _PeoplePageState extends State<PeoplePage> {
   }
 
   buildWordIndex() {
-    List<String> words = [
-      "↑",
-      "☆",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-      "#",
-    ];
     return Positioned(
       top: 0,
       right: 0,
       bottom: 0,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: words.map((word) => Text(word)).toList(),
+      child: Container(
+        alignment: Alignment.center,
+        child: GestureDetector(
+          onTapDown: (TapDownDetails details) {
+            moveToWord(details.localPosition.dy);
+          },
+          onPanUpdate: (DragUpdateDetails details) {
+            moveToWord(details.localPosition.dy);
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _indexWord
+                .map(
+                  (word) => SizedBox(
+                    width: WORD_SIZE + 20,
+                    height: WORD_SIZE,
+                    child: Center(
+                      child: Text(word),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
         ),
       ),
     );
   }
 
+  moveToWord(double y) {
+    int i = (y / WORD_SIZE).floor();
+    String word = _indexWord[i];
+    double scrollHeight = _controller.position.maxScrollExtent;
+
+    if (_heights[word] != null) {
+      _controller.jumpTo(Math.min(_heights[word]["top"], scrollHeight));
+    }
+  }
+
   buildWordBlock(String word) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+      height: 30.0,
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Text(
         word,
         style: TextStyle(color: Colors.black54),
