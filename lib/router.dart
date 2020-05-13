620e69bf73e2235d70d22aa2de67390271e82585
Simon-Bin <2632003786@qq.com>
2020-05-13 22:49:50 +0800

    :bug: 修复了路由的bug

--- lib/router.dart
+++ lib/router.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:simonchat/tabs/chat.dart';
-
+import 'package:simonchat/layout.dart';
 import 'message/message_page.dart';
 
 class SimonRouter {
   static String initialRoute = "/";
 
   static Map<String, WidgetBuilder> routes = {
-    "/": (ctx) => MessagePage(),
-    "/login": (ctx) => ChatPage(),
+    "/": (ctx) => LayoutPage(),
+    "/message": (ctx) => MessagePage(),
   };
 
-  static Future goToCameraPage(BuildContext context) {
+  static Future goToMessagePage(BuildContext context) {
     return Navigator.of(context).pushNamed('/camera');
   }
 }
